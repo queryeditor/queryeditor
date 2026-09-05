21ff474c15b330bee3bc5506a6ff3fe76c358dc8
Daustinn <daustinn.music@gmail.com>
2026-09-05 15:00:50 -0500

    seo: improve meta tags, alts, h1, JSON-LD, and sitemap

--- apps/web/src/pages/sitemap.xml.ts
+++ apps/web/src/pages/sitemap.xml.ts
@@ -12,12 +12,15 @@ export const GET: APIRoute = async ({ site }) => {
     '/legal/eula'
   ]
 
+  const today = new Date().toISOString().split('T')[0]
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${routes
   .map(
     (route) => `  <url>
     <loc>${baseUrl}${route}</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>${route === '' ? '1.0' : '0.8'}</priority>
   </url>`
